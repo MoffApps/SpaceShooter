f4be6e81e162b2ab16279d72fd8dccc02eb92ded
MoffApps <tony.moffatt2@hotmail.com>
2016-02-01 11:36:39 +0000

    Second Commit

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mover : MonoBehaviour {
+
+
+
+	void Start () {
+        Rigidbody rigidbody = GetComponent<Rigidbody>();
+        rigidbody.velocity = transform.forward * 20;
+    }
+	
+
+	
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rigidbody;
+
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+
+    public GameObject shot;
+    public Transform shotSpawn;
+  
+
+    private float nextFire;
+
+    void Update()
+    {
+        if (Input.GetButton("Fire1"))// && Time.time > nextFire
+        {
+            nextFire = Time.time + 1;
+
+            Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+        }
+    }
+
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+       
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+     
+        rigidbody.velocity = movement * 5;
+
+        rigidbody.position = new Vector3
+         (
+             Mathf.Clamp(rigidbody.position.x, -6, 6),
+             0.0f,
+             Mathf.Clamp(rigidbody.position.z,-4, 13)
+         );
+
+        rigidbody.rotation = Quaternion.Euler(0.0f, 0.0f, rigidbody.velocity.x * -10);
+
+    }
+}
