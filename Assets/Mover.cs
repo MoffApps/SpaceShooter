ff0e3e4e20248db208d982b3546d590083fa0b63
MoffApps <tony.moffatt2@hotmail.com>
2016-02-01 09:51:30 +0000

    First commit
    Start of class for  01/02/2016

---
+++ Assets/Mover.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mover : MonoBehaviour {
+
+
+
+	// Use this for initialization
+	void Start () {
+        Rigidbody rigidbody = GetComponent<Rigidbody>();
+        rigidbody.velocity = transform.forward * 20;
+    }
+	
+	// Update is called once per frame
+	
+}
